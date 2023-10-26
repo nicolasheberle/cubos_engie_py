c5cc75bc93b522dabe3e55b5625cf5eb39a537a6
Nicolas Heberle <62620939+nicolasheberle@users.noreply.github.com>
2023-10-25 21:08:59 -0300

    Update exer.6.py

--- manipulação/exer.6.py
+++ manipulação/exer.6.py
@@ -4,7 +4,7 @@
 # Substitua todas as letras L de seu nome por um V "v"
 # Seu nome possui a letra p?
 
-nome = "Nicolas Marcolin Heberle"
+nome = 'Nicolas Silva Heberle'
 print(len(nome))
 print(nome.upper())
 print(nome.replace('l', 'v'))
