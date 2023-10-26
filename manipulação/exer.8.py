fe7fe7530abd9216b37c5a9db5a00af274796e55
Nicolas Heberle <62620939+nicolasheberle@users.noreply.github.com>
2023-10-25 21:34:14 -0300

    att exerc
    Novos exercícios

---
+++ manipulação/exer.8.py
@@ -0,0 +1,9 @@
+# Copie as variáveis do exercício anterior;
+# Declare a variável apelido com o seu apelido favorito como valor;
+# Aplique as variáveis na seguinte frase:
+#  "O meu nome é [PRIMEIRO NOME] [ÚLTIMO NOME], mas muitos me chamam de [APELIDO]"
+
+primeiro_nome = 'Nicolas'
+ultimo_nome = 'Heberle'
+apelido = 'Aluanz'
+print(f'O meu nome é {primeiro_nome} {ultimo_nome}, mas muitos me chamam de {apelido}')
---
+++ manipulação/exer.9.py
@@ -0,0 +1,14 @@
+# Exercício 09
+# Para saber se duas strings são iguais um dos métodos que temos é verificar o tamanho de caracteres de cada antes de fazer alguma
+# comparação. Utilizando esse método, descubra se as Strings abaixo tem a possibilidade de ser iguais a partir do tamanho de cada uma delas.
+# Caso 1
+str1 = 'n+MqY5X9b+DIh+9mQj LT89xDYy1NaA4tARNQmqFCr9cz+27J13qxoxrmtST9VabY4LdBaTUQkjTK+xFq5g35T+hrp3XRiAbuJLqnGJPx1Z8T3WwAo8u1Hr3qR6u7vZxS'
+str2 = 'n+MqY5X9b+DIh+9mQj LT89xDYy1NaA4tARNQmqFCr9cz+27J13qxoxrmtST9VabY4LdBaTUQkjTK+xFq5g35T+hrp3XRiAbuJLqnGJPx1Z8T3WwAo8u1Hr3qR6u7vZxS'
+print(len(str1))
+print(len(str2))
+
+# Caso 2
+str3 = 'n+MqY5X9b+DIh+9mQjLT89xDylNaA4tARNQmqFCr9cz+27313qxoxrmtST9VabY4LdBaTUQkjTK+xFq5g35T+hrp3XRiAbuJLqnGJPx1Z8T3WwAo8u1Hr3qR6u7vZxs'
+str4 = 'n+MqY5X9b+DIh+9mQj LT89xDYy1NaA4tARNQmqFCr9cz+27J13qxoxrmtST9VabY4LdBaTUQkjTK+xFq5g35T+hrp3XRiAbuJLqnGJPx1Z8T3WwAo8u1Hr3qR6u7vZxSO'
+print(len(str3))
+print(len(str4))
